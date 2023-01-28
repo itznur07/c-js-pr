58b7aebba6c908be9986cba6a2f787c3adb8f319
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-28 07:19:44 +0600

    problem1 solve

---
+++ index.js
@@ -0,0 +1,14 @@
+let fruits = ['Apple', 'Banana', 'Orange']
+
+let bananaIndex = fruits.indexOf('Banana');
+console.log(bananaIndex);
+
+fruits[1] = "Mango"
+
+console.log(fruits);
+
+fruits.pop('Orange')
+console.log(fruits);
+
+fruits.push('Watermelon')
+console.log(fruits);
